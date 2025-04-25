4e57520a4790961c59e09c71e5c55c6f15a1b4a3
Кирилл <kotomanov.1990@mail.ru>
2025-04-25 19:47:18 +1000

    Changes for forked repository

--- OPN_PROJECT.cpp
+++ OPN_PROJECT.cpp
@@ -8,11 +8,16 @@ float Addition(float x, float y) {
 	return x + y;
 }
 
+float Multiply(float x, float y) {
+	return x * y;
+}
+
 int main() {
 	setlocale(LC_ALL, "RUS");
 	float x, y;
 	std::cout << "Введите x и y: ";
 	std::cin >> x >> y;
 	std::cout << "Разность x-y = " << Substraction(x, y) << std::endl;
-	std::cout << "Сумма x+y = " << Addition(x, y);
+	std::cout << "Сумма x+y = " << Addition(x, y) << std::endl;
+	std::cout << "Произведение x*y = " << Multiply(x, y) << std::endl;
 }

96cee2774706ab509480089e6971314fbed89fd2
Кирилл <kotomanov.1990@mail.ru>
2025-04-25 19:31:05 +1000

    Implemented new feature

--- OPN_PROJECT.cpp
+++ OPN_PROJECT.cpp
@@ -4,9 +4,15 @@ float Substraction(float x, float y) {
 	return x - y;
 }
 
+float Addition(float x, float y) {
+	return x + y;
+}
+
 int main() {
+	setlocale(LC_ALL, "RUS");
 	float x, y;
 	std::cout << "Введите x и y: ";
 	std::cin >> x >> y;
-	std::cout << "Разность x-y = " << Substraction(x, y);
+	std::cout << "Разность x-y = " << Substraction(x, y) << std::endl;
+	std::cout << "Сумма x+y = " << Addition(x, y);
 }

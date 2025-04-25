32c1f8c0046a651b133b60fdcf7ab40f0cd0390f
Кирилл <kotomanov.1990@mail.ru>
2025-04-25 18:33:33 +1000

    Initial commit with gitingnore and README

---
+++ OPN_PROJECT.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+float Substraction(float x, float y) {
+	return x - y;
+}
+
+int main() {
+	float x, y;
+	std::cout << "Введите x и y: ";
+	std::cin >> x >> y;
+	std::cout << "Разность x-y = " << Substraction(x, y);
+}

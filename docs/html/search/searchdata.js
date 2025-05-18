e5722241f4ef025c50e3013769b7847c581ea3b7
Laba <kotomanov.1990@mail.ru>
2025-05-18 17:08:18 +1000

    Make doxygen

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "ип",
+  1: "ип"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
